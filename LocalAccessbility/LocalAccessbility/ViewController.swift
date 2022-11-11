7beceef4fbd66ceefa12c0d388c9c2f6e4dc67af
dddowon <dowon4095@naver.com>
2022-11-11 11:25:38 +0900

    localization 실습

--- LocalAccessbility/LocalAccessbility/ViewController.swift
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var dateTimeLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
         
@@ -16,7 +17,7 @@ class ViewController: UIViewController {
         let bottomLabel = UILabel()
         bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
         
-        bottomLabel.text = "Touch the button below!"
+        bottomLabel.text = "Touch the button below!".localizing()
         
         bottomLabel.font = UIFont.systemFont(ofSize: 20)
         
@@ -33,7 +34,7 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
         
-        button.setTitle("Touch Me!", for: .normal)
+        button.setTitle("Touch Me!".localizing(), for: .normal)
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         
@@ -42,12 +43,36 @@ class ViewController: UIViewController {
         view.addSubview(button)
     }
     
+    private func addDate() {
+        let date = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short)
+        
+        dateTimeLabel.text = date
+    }
+    
+    private func currency() {
+        let locale = Locale.current
+        let price = 3000.33 as NSNumber
+        let formatter = NumberFormatter()
+        
+        formatter.numberStyle = .currency
+        formatter.currencyCode = locale.currencyCode
+        formatter.locale = locale
+        
+        currencyLabel.text = formatter.string(from: price)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addBottomLabel()
         addButton()
-        
-        dateTimeLabel.text = Date().description
+        welcomeLabel.text = "Welcome to yagom-academy!".localizing()
+        addDate()
+        currency()
     }
 }
 
+extension String {
+    func localizing(_ comment: String = "") -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+}
